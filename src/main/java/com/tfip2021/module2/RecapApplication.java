f52124f44ced9d237480eced2a7741e872e7f28f
ngchinhow <ngchinhow@gmail.com>
2022-01-12 09:36:00 +0800

    Initialize project

---
+++ src/main/java/com/tfip2021/module2/RecapApplication.java
@@ -0,0 +1,13 @@
+package com.tfip2021.module2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapApplication.class, args);
+	}
+
+}
